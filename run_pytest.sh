c97dba2d20c0f41c6a7447b21477ed530d4f96bf
Feeph Aifeimei <55798703+feeph@users.noreply.github.com>
2024-06-07 06:38:38 +0200

    WIP Fri  7 Jun 06:38:38 CEST 2024

--- run_pytest.sh
+++ run_pytest.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+#
+# initialize virtual environment (if needed) and run unittests
+# (assumes Python 3.11 is used)
+#
+# usage:
+#   ./run_pytest.sh
+#   TEST_EMC2101_CHIP=y ./run_pytest.sh
+#   TEST_EMC2101_CHIP=y TEST_EMC2101_SENSOR=y ./run_pytest.sh
+
+if [ ! -d .venv ] ; then
+    python3 -m venv .venv --prompt="`python3 --version|cut -d'.' -f1,2`"
+    .venv/bin/pip install --upgrade pip
+    .venv/bin/pip install -r requirements.txt
+fi
 
 PYTHONPATH=src:.venv/lib/python3.11/site-packages/ pytest tests/
